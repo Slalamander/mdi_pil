dfb16ae4e3546675654c4c81e73eb6a846bc0766
Sander <sanderderidder@hotmail.nl>
2025-01-09 15:05:42 +0100

    Bump version

--- mdi_pil/constants.py
+++ mdi_pil/constants.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 from typing import TypeVar, Union
 
-__version__ = "1.1.6"
+__version__ = "1.1.7"
 
 MDI_FONT_FILE = "materialdesignicons-webfont.ttf"
 MDI_INDEX_FILE = "_variables.scss"
